55018dfa48ba5e1d0401e07f0f5d2c3026b00cba
borauyar <bora.uyar@mdc-berlin.de>
2022-10-02 18:30:03 +0200

    simplify code to extract statistics, save variable importance as Matrices

--- src/analyse_models.R
+++ src/analyse_models.R
@@ -1,113 +1,55 @@
 # analyse models built for drug response
 ## libraries & functions
-if (!require("tidyverse")) install.packages("tidyverse") else library(tidyverse)
-if (!require("foreach")) install.packages("foreach") else library(foreach)
-if (!require("tictoc")) install.packages("tictoc") else library(tictoc)
 if (!require("data.table")) install.packages("data.table") else library(data.table)
-if (!require("furrr")) install.packages("furrr") else library(furrr)
-if (!require("purrr")) install.packages("purrr") else library(purrr)
-#utility_functions <- file.path("F_auxiliary.R")
 source("src/F_auxiliary.R")
+
+args <- commandArgs(trailingOnly = TRUE)
+
+folder <- args[1] # folder containing caret.RDS files (modeling results)
 
 # start the parallelization
 message(date()," => assembling model stats")
-p.out <- '/data/local/buyar/arcas/panel_sequencing_paper/multiomics_vs_panelseq/test'
-outdir <- 'CCLE'
 
-files <- dir(file.path(p.out, outdir), ".caret.RDS$", full.names = T)
+files <- dir(folder, ".caret.RDS$", full.names = T)
 
-cl <- parallel::makeCluster(20)
-parallel::clusterExport(cl = cl, varlist = c('files', 'p.out', 'outdir'))
-## assemble model stats
-dt <- do.call(
-  rbind,
-  pbapply::pblapply(cl = cl, files,
-    function(f) {
-      x <- readRDS(f)
-      drug <- gsub(".caret.RDS$", "", basename(f))
-      dt <- rbind(
-        x$panel$panel.stats,
-        x$panel$panel.stats.pca,
-        x$panel$panel.stats.rf,
-        x$panel$panel.stats.rf.pca,
-        x$mo$mo.stats,
-        x$mo$mo.stats.pca,
-        x$mo$mo.stats.rf,
-        x$mo$mo.stats.rf.pca
-      )
-      dt$total_sample_count <- rep(
-        c(
-          x$mo$total_sample_count,
-          x$panel$total_sample_count
-        ),
-        4
-      )
-      dt$training_sample_count <- rep(
-        c(
-          x$mo$training_sample_count,
-          x$panel$training_sample_count
-        ),
-        4
-      )
-      dt$testing_sample_count <- rep(
-        c(
-          x$mo$testing_sample_count,
-          x$panel$testing_sample_count
-        ),
-        4
-      )
-      dt$type <- rep(c("panel", "mo"), each = 4)
-      dt$pp <- rep(c("scale+nzv", "scale+nzv+pca"), 4)
-      dt$model <- rep(c(rep("glm", 2),rep("rf", 2)),2)
-      dt$drug <- rep(drug, 8)
-      return(dt)
-    }
-  )
-)
+# read results files (includes fit models and statistics)
+cl <- parallel::makeCluster(5)
+parallel::clusterExport(cl = cl, varlist = c('files'))
+results <- pbapply::pblapply(cl = cl, files, function(f) {
+  x <- readRDS(f)
+})
 parallel::stopCluster(cl)
-saveRDS(dt, file = file.path(p.out, outdir, "caret.stats.RDS"))
+names(results) <- gsub(".caret.RDS$", "",basename(files))
 
-## Estimate feature importance glmnet
-plan(multisession, workers = 4)
-files[1:4] %>%
-  future_map(
-    .,
-    ~ caret::varImp(readRDS(.x)$mo$mo.fit)$importance
-  ) -> l.varimp
-names(l.varimp) <- gsub(".caret.RDS$", "", basename(files))
-                        
-purrr::map2(
-  l.varimp,
-  names(l.varimp),
-  ~ .x %>%
-    as_tibble(rownames = "Feature") %>%
-    mutate(drug = .y)
-) %>%
-  purrr::reduce(., rbind) -> t.varimp
-write_tsv(t.varimp, file.path(p.out, outdir, "varImp.tsv"))
+# get stats
+stats <- do.call(rbind, lapply(results, function(x) {
+  do.call(rbind, lapply(x, function(y) y$stats))
+}))
+saveRDS(stats, file = file.path(folder, "caret.stats.RDS"))
 
+# go through each multiomics model and extract variable importance metrics
+get_varImp <- function(results, algorithm, type = 'mo.fit') {
+  imp <- sapply(simplify = F, names(results), function(drug) {
+    message(drug)
+    imp <- caret::varImp(results[[drug]][[algorithm]][[type]])[['importance']]
+    dt <- data.table('feature' = rownames(imp), 'val' = imp$Overall)
+    colnames(dt)[2] <- drug
+    return(dt)
+  })
+  imp <- Reduce(function(x, y) merge(x, y, all=TRUE, by = 'feature'), imp)
+  # convert NA values to zero
+  imp[is.na(imp)] <- 0
+  return(imp)
+}
 
-## Estimate feature importance rf
-list.files(file.path(p.out, outdir),
-           pattern = ".caret.RDS",
-           full.names = T
-) %>%
-  future_map(
-    .,
-    ~ caret::varImp(readRDS(.x)$mo$mo.fit.rf)$importance
-  ) -> l.varimp
-names(l.varimp) <- list.files(file.path(p.out, outdir),
-                              pattern = ".caret.RDS",
-                              full.names = F
-) %>%
-  basename() %>%
-  str_remove(., ".caret.RDS")
-purrr::map2(
-  l.varimp,
-  names(l.varimp),
-  ~ .x %>%
-    as_tibble(rownames = "Feature") %>%
-    mutate(drug = .y)
-) %>%
-  purrr::reduce(., rbind) -> t.varimp
-write_tsv(t.varimp, file.path(p.out, outdir, "varImp_rf.tsv"))
+# for RF:
+rf_imp <- get_varImp(results, 'rf', 'mo.fit')
+write.table(rf_imp, file = file.path(folder, "feature_importance.RF.tsv"), sep = '\t',
+            quote = F, row.names = F)
+
+# for GLM
+glm_imp <- get_varImp(results, 'glm', 'mo.fit')
+write.table(glm_imp, file = file.path(folder, "feature_importance.GLM.tsv"), sep = '\t',
+            quote = F, row.names = F)
+
+message(date(), " => finished assembling the model statistics")

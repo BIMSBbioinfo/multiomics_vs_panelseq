62f2c3ef95408899b8f00208f55abafa45874cac
borauyar <bora.uyar@mdc-berlin.de>
2022-10-03 10:43:47 +0200

    update script to avoid keeping all results in memory

--- src/analyse_models.R
+++ src/analyse_models.R
@@ -1,6 +1,7 @@
 # analyse models built for drug response
 ## libraries & functions
 if (!require("data.table")) install.packages("data.table") else library(data.table)
+library(pbapply)
 source("src/F_auxiliary.R")
 
 args <- commandArgs(trailingOnly = TRUE)
@@ -12,43 +13,49 @@ message(date()," => assembling model stats")
 
 files <- dir(folder, ".caret.RDS$", full.names = T)
 
-# read results files (includes fit models and statistics)
-cl <- parallel::makeCluster(5)
-parallel::clusterExport(cl = cl, varlist = c('files'))
-results <- pbapply::pblapply(cl = cl, files, function(f) {
-  x <- readRDS(f)
-})
-parallel::stopCluster(cl)
-names(results) <- gsub(".caret.RDS$", "",basename(files))
+pbo = pbapply::pboptions(type="txt")
 
-# get stats
-stats <- do.call(rbind, lapply(results, function(x) {
+message(date()," => collating stats")
+cl <- parallel::makeCluster(30)
+parallel::clusterExport(cl = cl, varlist = c('files'))
+stats <- do.call(rbind, pbapply::pblapply(cl = cl, files, function(f) {
+  x <- readRDS(f)
   do.call(rbind, lapply(x, function(y) y$stats))
 }))
+parallel::stopCluster(cl)
 saveRDS(stats, file = file.path(folder, "caret.stats.RDS"))
 
-# go through each multiomics model and extract variable importance metrics
-get_varImp <- function(results, algorithm, type = 'mo.fit') {
-  imp <- sapply(simplify = F, names(results), function(drug) {
-    message(drug)
-    imp <- caret::varImp(results[[drug]][[algorithm]][[type]])[['importance']]
-    dt <- data.table('feature' = rownames(imp), 'val' = imp$Overall)
-    colnames(dt)[2] <- drug
-    return(dt)
-  })
-  imp <- Reduce(function(x, y) merge(x, y, all=TRUE, by = 'feature'), imp)
-  # convert NA values to zero
-  imp[is.na(imp)] <- 0
-  return(imp)
-}
 
-# for RF:
-rf_imp <- get_varImp(results, 'rf', 'mo.fit')
+message(date()," => collating variable importance metrics")
+# go through each multiomics model and extract variable importance metrics
+# memory intensive job is to read the RDS files, so, we don't keep the contents in memory
+cl <- parallel::makeCluster(30)
+parallel::clusterExport(cl = cl, varlist = c('files'))
+imp <- pbapply::pblapply(cl = cl, files, function(f) {
+  require(data.table)
+  x <- readRDS(f)
+  drug <- gsub(".caret.RDS$", "", basename(f))
+  imp <- caret::varImp(x[['rf']][['mo.fit']])[['importance']]
+  dt.rf <- data.table('feature' = rownames(imp), 'val' = imp$Overall)
+  colnames(dt.rf)[2] <- drug
+  # parse glm
+  imp <- caret::varImp(x[['glm']][['mo.fit']])[['importance']]
+  dt.glm <- data.table('feature' = rownames(imp), 'val' = imp$Overall)
+  colnames(dt.glm)[2] <- drug
+  list('rf' = dt.rf, 'glm' = dt.glm)
+})
+parallel::stopCluster(cl)
+
+rf_imp <- lapply(imp, function(x) x[['rf']])
+glm_imp <- lapply(imp, function(x) x[['glm']])
+
+rf_imp <- Reduce(function(x, y) merge(x, y, all=TRUE, by = 'feature'), rf_imp)
+rf_imp[is.na(rf_imp)] <- 0 # Convert NA to zero
 write.table(rf_imp, file = file.path(folder, "feature_importance.RF.tsv"), sep = '\t',
             quote = F, row.names = F)
 
-# for GLM
-glm_imp <- get_varImp(results, 'glm', 'mo.fit')
+glm_imp <- Reduce(function(x, y) merge(x, y, all=TRUE, by = 'feature'), glm_imp)
+glm_imp[is.na(glm_imp)] <- 0 # Convert NA to zero
 write.table(glm_imp, file = file.path(folder, "feature_importance.GLM.tsv"), sep = '\t',
             quote = F, row.names = F)
 

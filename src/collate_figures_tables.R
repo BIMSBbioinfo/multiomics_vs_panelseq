ea73fc5abd297b9d5785ff6175dfaf1a1d8a11a2
İrem Gündüz <irembgunduz@gmail.com>
2022-10-11 22:45:03 +0200

    issues with figures 1-7

--- src/collate_figures_tables.R
+++ src/collate_figures_tables.R
@@ -76,7 +76,8 @@ p1 <- ggscatter(dtc, x = 'panel', y = 'multiomics') +
 # use "ranger" importance metrics 
 # get a summary of top gex 
 dt <- stats[model == 'ranger'][ppOpts == 'center+scale+nzv'][dataset == 'CCLE']
-dtc <- dcast.data.table(dt, drugName ~ type, value.var = 'Rsquare')
+dt <- dt %>% dplyr::rename(Rsquared = Rsquare)
+dtc <- dcast.data.table(dt, drugName ~ type, value.var = 'Rsquared')
 dtc$improvement <- dtc$multiomics - dtc$panel
 # compute percentage of "gex" features in top informative features for CCLE
 # use ranger importance metrics 
@@ -109,7 +110,7 @@ p2 <- ggscatter(dtc, x = 'improvement', y = 'count_gex', fill = 'multiomics',
   labs(
     x = "Multiomics improvement (CCLE)",
     y = "% of gene expression features\namong top 100 Features",
-    fill = "Rsquare"
+    fill = "Rsquared"
   ) +
   theme(legend.position = 'right')
 
@@ -117,36 +118,40 @@ p2 <- ggscatter(dtc, x = 'improvement', y = 'count_gex', fill = 'multiomics',
 
 ## make PDX plot (boxplot per drug with significance labels)
 dt <- stats[dataset == 'PDX'][model == 'ranger'][ppOpts == 'center+scale+nzv']
-dtc <- dcast(dt, drugName + run ~ type, value.var = 'Rsquare')
+dt <- dt %>% dplyr::rename(Rsquared = Rsquare)
+dtc <- dcast(dt, drugName + run ~ type, value.var = 'Rsquared')
 stars <- do.call(rbind, lapply(split(dtc, dtc$drugName), function(x) {
   pval <- wilcox.test(x$multiomics, x$panel, alternative = 'greater')[['p.value']]
   data.table('drugName' = x$drugName[1], 
              'pval' = pval,
              'star' = gtools::stars.pval(pval))
 }))
-p3 <- ggplot(dt, aes( x = drugName, y = Rsquare)) + 
+p3 <- ggplot(dt, aes( x = drugName, y = Rsquared)) + 
   geom_boxplot(aes(fill = type), outlier.shape = NA) + 
-  geom_text(data = stars, aes(x = drugName, y = 0.5, label = star)) +
+  geom_text(data = stars, aes(x = drugName, y = 0.45, label = star)) +
   scale_fill_brewer(type = 'qual', palette = 6) +
   facet_grid(~ dataset) +
+  labs(x = "Drugs", 
+       y = "Rsquared")+
   theme(legend.title = element_blank(), legend.position = 'top') +
   coord_flip()
 
 
 # collate Figure 1 
-layout <- 
-"AAACC
-AAACC
-BBBCC
-"
-p <- p1 + p2 + p3 + plot_layout(design = layout) + plot_annotation(tag_levels = 'A')
+#layout <- 
+#  "AAACC
+#AAACC
+#BBBCC
+#"
+#p <- p1 + p2 + p3 + plot_layout(design = layout) + plot_annotation(tag_levels = 'A')
 
-ggsave(filename = file.path(folder, 'figure1.pdf'), plot = p, width = 13.76, height = 7.44)
+#ggsave(filename = file.path(folder, 'figure1.pdf'), plot = p, width = 13.76, height = 7.44)
 
 # supp. figure 1: => we say why we chose RF for main figure
 # comparison of methods (RF/glmnet/svm) and ppopts (with/without pca)
+stats <- stats %>% dplyr::rename(Rsquared = Rsquare)
 plots <- lapply(c('CCLE', 'beatAML'), function(ds) {
-  ggboxplot(stats[dataset == ds], x = 'type', y = 'Rsquare', 
+  ggboxplot(stats[dataset == ds], x = 'type', y = 'Rsquared', 
             add = 'jitter', color = 'type') +
     facet_grid(ppOpts ~ model) + stat_compare_means() + 
     scale_color_brewer(type = 'qual', palette = 6) +
@@ -162,8 +167,8 @@ ggsave(filename = file.path(folder, 'figure_S1.pdf'), plot = p,
 # drug classes by improvement 
 # assemble a table
 dt.dcl <- stats[dataset == "CCLE" & ppOpts == "center+scale+nzv" & model == "ranger"]
-dt.dcl <- dt.dcl[, .(Rsquare, drugName, type)]
-dt.dcl <- dcast(dt.dcl, drugName ~ type, value.var = "Rsquare")
+dt.dcl <- dt.dcl[, .(Rsquared, drugName, type)]
+dt.dcl <- dcast(dt.dcl, drugName ~ type, value.var = "Rsquared")
 dt.dcl[, impr := multiomics - panel]
 dt.dcl <- dr.cls[dt.dcl, on = "drugName"]
 # select 8 drug classes that are the most represented in the current dataset
@@ -185,79 +190,90 @@ moa.ord <- t.tmp %>%
   pull(MOA)
 
 # assemblt the plot
-p <- t.tmp %>% 
+p4 <- t.tmp %>% 
   mutate(MOA = factor(MOA, levels = c(moa.ord))) %>% 
   ggplot(data = ., aes(x = impr, y = MOA, fill = stat(x))) + 
   geom_density_ridges_gradient(scale = 2, 
                                rel_min_height = 0.01, 
                                gradient_lwd = 1,
                                bandwidth = 0.03) + 
-  scale_fill_gradient2(low = "black", mid = "gray", high = "red") +
+  scale_fill_gradient2(low = "black", mid = "gray", high = "blue") +
   geom_vline(xintercept = 0) + 
   geom_vline(xintercept = 0.053, 
              linetype = "dashed") + 
   scale_x_continuous(expand = c(0.07, -0.01)) +
-  labs(x = "Multiomics improvement (CCLE)", 
+  labs(x = "Average Multiomics Improvement (CCLE)", 
        y = "Drug mechanism of action") +
   guides(fill = guide_colourbar(barwidth = 26, 
                                 barheight = 1, 
                                 direction = "horizontal", 
                                 title = "", label = F, ticks = T)) + 
   theme(legend.position = "bottom", 
-  axis.title.x = element_text(vjust = -1.2))
+        axis.title.x = element_text(vjust = -1.2))
 
-ggsave(filename = file.path(folder, 'figure_2.pdf'), plot = p, 
-       width = 9, height = 4.96)
+#ggsave(filename = file.path(folder, 'figure_2.pdf'), plot = p4,width = 9, height = 4.96)
 
+#for plotting
+region <- function(row, col){
+  viewport(layout.pos.row = row, layout.pos.col = col)
+} 
 
+pdf(paste0(folder,"/figure_1.pdf"),width = 15,height = 8)
+grid.newpage()
+pushViewport(viewport(layout = grid.layout(nrow = 5, ncol = 5)))
+print(p1+labs(tag = "A"), vp = region(row = 1:3, col = 1:3))   # Span over two columns
+print(p2+labs(tag = "B"), vp = region(row = 1:2, col = 4:5))
+print(p4+labs(tag = "C"), vp = region(row = 4:5, col = 1:3))
+print(p3+labs(tag = "D") , vp = region(row = 3:5, col = 4:5))
+dev.off()
 
 # supplementary figure 2: 
 # drug classes by improvement (glmnet and svm)
 lapply(c("glmnet", "svmRadial"), 
        function(model_name) {
-        dt.dcl <- stats[dataset == "CCLE" & ppOpts == "center+scale+nzv" & model == model_name]
-        dt.dcl <- dt.dcl[, .(Rsquare, drugName, type)]
-        dt.dcl <- dcast(dt.dcl, drugName ~ type, value.var = "Rsquare")
-        dt.dcl[, impr := multiomics - panel]
-        dt.dcl <- dr.cls[dt.dcl, on = "drugName"]
-        # select 8 drug classes that are the most represented in the current dataset
-        dt.dcl %>% 
-          tabyl(MOA) %>% 
-          drop_na() %>%
-          arrange(desc(n)) %>% 
-          filter(n > 5) %>% 
-          pull(MOA) -> v.tmp
-        # filter the plotting table to include only those
-        dt.dcl %>% 
-          filter(MOA %in% v.tmp) -> t.tmp
-        # order drug classes based on average improvement in MOs
-        moa.ord <- t.tmp %>% 
-          group_by(MOA) %>% 
-          summarise(med_impr = median(impr), 
-                    avg_impr = mean(impr), .groups = "drop") %>% 
-          arrange(avg_impr) %>% 
-          pull(MOA)
-        # assemblt the plot
-        t.tmp %>% 
-          mutate(MOA = factor(MOA, levels = c(moa.ord))) %>% 
-          ggplot(data = ., aes(x = impr, y = MOA, fill = stat(x))) + 
-          geom_density_ridges_gradient(scale = 2, 
-                                      rel_min_height = 0.01, 
-                                      gradient_lwd = 1,
-                                      bandwidth = 0.03) + 
-          scale_fill_gradient2(low = "black", mid = "gray", high = "red") +
-          geom_vline(xintercept = 0) + 
-          geom_vline(xintercept = 0.053, 
-                    linetype = "dashed") + 
-          scale_x_continuous(expand = c(0.07, -0.01)) +
-          labs(x = paste0("Multiomics improvement (CCLE,", model_name, ")"), 
-               y = "Drug mechanism of action") +
-          guides(fill = guide_colourbar(barwidth = 26, 
-                                        barheight = 1, 
-                                        direction = "horizontal", 
-                                        title = "", label = F, ticks = T)) + 
-          theme(legend.position = "bottom", 
-                axis.title.x = element_text(vjust = -1.2))
+         dt.dcl <- stats[dataset == "CCLE" & ppOpts == "center+scale+nzv" & model == model_name]
+         dt.dcl <- dt.dcl[, .(Rsquared, drugName, type)]
+         dt.dcl <- dcast(dt.dcl, drugName ~ type, value.var = "Rsquared")
+         dt.dcl[, impr := multiomics - panel]
+         dt.dcl <- dr.cls[dt.dcl, on = "drugName"]
+         # select 8 drug classes that are the most represented in the current dataset
+         dt.dcl %>% 
+           tabyl(MOA) %>% 
+           drop_na() %>%
+           arrange(desc(n)) %>% 
+           filter(n > 5) %>% 
+           pull(MOA) -> v.tmp
+         # filter the plotting table to include only those
+         dt.dcl %>% 
+           filter(MOA %in% v.tmp) -> t.tmp
+         # order drug classes based on average improvement in MOs
+         moa.ord <- t.tmp %>% 
+           group_by(MOA) %>% 
+           summarise(med_impr = median(impr), 
+                     avg_impr = mean(impr), .groups = "drop") %>% 
+           arrange(avg_impr) %>% 
+           pull(MOA)
+         # assemblt the plot
+         t.tmp %>% 
+           mutate(MOA = factor(MOA, levels = c(moa.ord))) %>% 
+           ggplot(data = ., aes(x = impr, y = MOA, fill = stat(x))) + 
+           geom_density_ridges_gradient(scale = 2, 
+                                        rel_min_height = 0.01, 
+                                        gradient_lwd = 1,
+                                        bandwidth = 0.03) + 
+           scale_fill_gradient2(low = "black", mid = "gray", high = "red") +
+           geom_vline(xintercept = 0) + 
+           geom_vline(xintercept = 0.053, 
+                      linetype = "dashed") + 
+           scale_x_continuous(expand = c(0.07, -0.01)) +
+           labs(x = paste0("Multiomics improvement (CCLE,", model_name, ")"), 
+                y = "Drug mechanism of action") +
+           guides(fill = guide_colourbar(barwidth = 26, 
+                                         barheight = 1, 
+                                         direction = "horizontal", 
+                                         title = "", label = F, ticks = T)) + 
+           theme(legend.position = "bottom", 
+                 axis.title.x = element_text(vjust = -1.2))
        }) -> l.p
 
 
@@ -274,11 +290,11 @@ p.ven <- ggplot(data = t.vnt.sort, aes(x = VENETOCLAX, y = feature)) +
   geom_col(aes(fill = colvar), size = 0.5) + 
   scale_fill_brewer(type = 'qual', palette = 6) +
   labs(y = "", x = "Venetoclax feature importance", fill = "Feature type") + 
-  theme(legend.position = c(0.7, 0.65))
+  theme(legend.position = c(0.75, 0.55))
 
 # collate supplementary figure 2 
 layout <- 
-"AAAACC
+  "AAAACC
  BBBBCC"
 p <- l.p[[1]] + l.p[[2]] + p.ven + plot_layout(design = layout) + plot_annotation(tag_levels = 'A')
 ggsave(filename = file.path(folder, 'figure_S2.pdf'), plot = p, 
@@ -321,8 +337,8 @@ OUT <- createWorkbook()
 lapply(c("CCLE", "beatAML"), function(Dataset) {
   lapply(c("ranger", "glmnet", "svmRadial"), function(Model) {
     dt.dcl <- stats[dataset == Dataset & ppOpts == "center+scale+nzv" & model == Model]
-    dt.dcl <- dt.dcl[, .(Rsquare, drugName, type)]
-    dt.dcl <- dcast(dt.dcl, drugName ~ type, value.var = "Rsquare")
+    dt.dcl <- dt.dcl[, .(Rsquared, drugName, type)]
+    dt.dcl <- dcast(dt.dcl, drugName ~ type, value.var = "Rsquared")
     dt.dcl[, impr := multiomics - panel]
     dt.dcl[, drugName := str_to_upper(drugName)]
     dt.dcl <- dr.cls[dt.dcl, on = "drugName"]
@@ -336,9 +352,4 @@ saveWorkbook(OUT, file.path(folder, "SupplementaryTable3.xlsx"), overwrite = T)
 
 
 message(date(), " => Finished collating figures and tables")
-
-
-
-
-
 
